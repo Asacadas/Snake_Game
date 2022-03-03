4bda4d8d1a56500e0290ed947fdebd1615e1f5e0
Asacadas <nielsenrochamaciel@gmail.com>
2022-03-03 00:18:48 -0300

    adição de comida, quando alimentado, a cobra cresce

--- js/script.js
+++ js/script.js
@@ -8,10 +8,14 @@ snake[0] = {
 }
 
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
-    context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 16 * box, 16 * box);
+    context.fillStyle = "yellow";
+    context.fillRect(0, 0, 16 * box, 16 * box); //deixa no 1 que vira um paint mó massa
 }
 
 function criarCobrinha(){
@@ -19,6 +23,11 @@ function criarCobrinha(){
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box)
 }
 
 document.addEventListener("keydown", update);
@@ -39,6 +48,7 @@ function iniciarJogo() {
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -48,7 +58,14 @@ function iniciarJogo() {
     if (direction == "up") snakeY += box;
     if (direction == "down") snakeY -= box;
      
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    } else {
+      food.x = Math.floor(Math.random() * 15 + 1) * box,
+      food.y = Math.floor(Math.random() * 15 + 1) * box  
+    }
+
+    //snake.pop();
 
     let newHead = {
         x: snakeX,
